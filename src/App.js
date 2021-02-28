85066941b9349150c3afb68c7fef0bc9dab321b5
Tav25 <turbin28@gmail.com>
2021-02-28 09:25:06 +0300

    init: init react app

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//      test
+//     </div>
+//   );
+// }
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+
+  render(){
+    return(
+      <div>game</div>
+    )
+  }
+}
+
+export default App;
