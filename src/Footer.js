8c732db89d2541dc496f4bdac2d03c0f2c71946a
Tav25 <turbin28@gmail.com>
2021-03-04 02:59:53 +0300

    feat: addded youtube

--- src/Footer.js
+++ src/Footer.js
@@ -5,6 +5,9 @@ import './Footer.css';
 export default function Footer() {
   return (
     <div className = "foterClass">
+      <Link href="https://youtu.be/q2WhtvKZsYo"  variant="body2">
+    ссылка на видео
+  </Link>
       <Link href="https://rs.school/js/"  variant="body2">
     {'rolling-scopes-school'}
   </Link>
